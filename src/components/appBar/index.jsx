3ba44cf5de95d3448ab23e5afcbc4cd35088ecee
Jr <jubayerrahman203@gmail.com>
2023-12-05 10:08:21 +0600

    2nd commit : in this part a simple app bar was created by me.

---
+++ src/components/appBar/index.jsx
@@ -0,0 +1,125 @@
+import styled from "styled-components";
+import { Container } from "../../utils/forStyled";
+import { useState } from "react";
+const AppBar = () => {
+  const [active, setactive] = useState(false);
+  return (
+    <nav>
+      <Container>
+        <Wrapper>
+          <div className="for__logo">
+            <h2>Zayex</h2>
+          </div>
+          <ForMenu vblty={active}>
+            <UnorderedList>
+              <ListItem>Home</ListItem>
+              <ListItem>Service</ListItem>
+              <ListItem>Portfolio</ListItem>
+              <ListItem>Contact</ListItem>
+            </UnorderedList>
+          </ForMenu>
+          <ForNavButtons>
+            <Button0>Get in Touch</Button0>
+            {/* <span class="material-symbols-outlined">close</span> */}
+            <span
+              class="material-symbols-outlined"
+              onClick={() => setactive((prev) => !prev)}
+            >
+              menu
+            </span>
+          </ForNavButtons>
+        </Wrapper>
+      </Container>
+    </nav>
+  );
+};
+
+export default AppBar;
+
+// ---------------styled-components--------------
+
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  margin: 1rem 0;
+  position: relative;
+  justify-content: space-between;
+  @media screen and (min-width: 710px) {
+    position: unset;
+  }
+
+  @media screen and (min-width: 980px) {
+    position: unset;
+  }
+`;
+
+const ForMenu = styled.div`
+  position: absolute;
+  top: 100%;
+  transform: ${(props) =>
+    props.vblty === true ? "translateX(0%)" : "translateX(-100%)"};
+  transition: transform ease 0.4s;
+  width: 100%;
+  height: 100vh;
+  padding: 2rem 0;
+  @media screen and (min-width: 710px) {
+    position: unset;
+    width: unset;
+    height: unset;
+  }
+
+  @media screen and (min-width: 980px) {
+    position: unset;
+    width: unset;
+    height: unset;
+  }
+`;
+
+const ListItem = styled.li`
+  text-transform: capitalize;
+  letter-spacing: 1px;
+  font-size: 1.2rem;
+  font-weight: 300;
+`;
+
+const UnorderedList = styled.ul`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 1rem;
+
+  @media screen and (min-width: 710px) {
+    flex-direction: row;
+    align-items: center;
+    gap: 1.6rem;
+  }
+  @media screen and (min-width: 980px) {
+    flex-direction: row;
+    align-items: center;
+    gap: 4rem;
+  }
+`;
+const Button0 = styled.button`
+  font-size: 1.2rem;
+  padding: 0.8rem 1.8rem;
+  background-color: black;
+  color: white;
+
+  @media screen and (min-width: 710px) {
+    font-size: 0.8rem;
+    padding: 0.8rem 1rem;
+  }
+
+  @media screen and (min-width: 980px) {
+    font-size: 1.2rem;
+    padding: 0.8rem 1.8rem;
+  }
+`;
+const ForNavButtons = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+
+  @media screen and (min-width: 980px) {
+  }
+`;
