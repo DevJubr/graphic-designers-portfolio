1be455161724b694884bc3270601c37baf3367c9
Jr <jubayerrahman203@gmail.com>
2023-12-06 18:06:26 +0600

    adding all respinsive

--- src/components/hero/Index.jsx
+++ src/components/hero/Index.jsx
@@ -1,16 +1,14 @@
 import React from "react";
 import { Container } from "../../utils/forStyled";
 import styled from "styled-components";
+import Model from "../../assets/model.jpeg";
 const HeroSection = () => {
   return (
     <section>
       <Container>
         <Wrapper>
           <ForImg>
-            <Img
-              src="https://chotourl.vercel.app/rNyZd8H_f"
-              alt="Zayedur rahman Sabbir"
-            />
+            <Img src={Model} alt="Zayedur rahman Sabbir" />
           </ForImg>
           <ForText>
             <Headline>
@@ -44,13 +42,22 @@ const ForImg = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
+  @media screen and (max-width: 710px) {
+  }
 `;
 
 const Img = styled.img`
-  width: 200px;
-  height: 200px;
+  width: 240px;
+  height: 240px;
   object-fit: cover;
   border-radius: 50%;
+  border-image: linear-gradient(138deg, #7aff60 9.86%, #ea00ff 104.95%) 1;
+  border-style: solid;
+  border-width: 4px;
+  @media screen and (max-width: 710px) {
+    width: 200px;
+    height: 200px;
+  }
 `;
 
 const Wrapper = styled.div`
@@ -58,24 +65,43 @@ const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
-  align-items: flex-start;
+  align-items: center;
   padding: 3rem 0;
   gap: 3rem;
+  @media screen and (max-width: 710px) {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    padding: 3rem 0;
+    gap: 3rem;
+  }
 `;
 
 const ForText = styled.div`
-  width: 100%;
+  width: 95%;
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
   gap: 1.5rem;
+  @media screen and (max-width: 710px) {
+  }
 `;
 
 const Headline = styled.div`
   h1 {
-    font-size: 2rem !important;
+    text-transform: capitalize;
+    font-size: 3.8rem !important;
     text-align: center;
+    line-height: 4.8rem;
+  }
+  @media screen and (max-width: 710px) {
+    h1 {
+      font-size: 2rem !important;
+      text-align: center;
+    }
   }
 `;
 
@@ -86,16 +112,30 @@ const ColorfulText = styled.h1`
   -moz-background-clip: text;
   -o-background-clip: text;
   color: transparent;
+  @media screen and (max-width: 710px) {
+  }
 `;
 
 const Subline = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
   p {
     text-transform: capitalize;
     letter-spacing: 1px;
     text-align: center;
-    width: 100%;
-    font-size: 0.8rem;
+    width: 50%;
+    font-size: 1rem;
     line-height: 1.22rem;
+  }
+  @media screen and (max-width: 710px) {
+    p {
+      text-align: center;
+      width: 100%;
+      font-size: 0.8rem;
+      line-height: 1.22rem;
+    }
   }
 `;
 
@@ -106,21 +146,35 @@ const ForButton = styled.div`
   flex-wrap: wrap;
   width: 100%;
   gap: 1rem;
+  @media screen and (max-width: 710px) {
+  }
 `;
 
 const Button = styled.button`
   text-transform: uppercase;
   letter-spacing: 1px;
   background: aliceblue;
-  font-size: 0.8rem;
+  font-size: 1rem;
   color: black;
-  padding: 0.8rem 1.2rem;
+  padding: 1rem 1.4rem;
   border-radius: 2rem;
   cursor: pointer;
+  @media screen and (max-width: 710px) {
+    text-transform: uppercase;
+    letter-spacing: 1px;
+    background: aliceblue;
+    font-size: 0.8rem;
+    color: black;
+    padding: 0.8rem 1.2rem;
+    border-radius: 2rem;
+    cursor: pointer;
+  }
 `;
 
 const WithborderButton = styled(Button)`
   background: none;
   color: aliceblue;
   border: 2px solid aliceblue;
+  @media screen and (max-width: 710px) {
+  }
 `;
