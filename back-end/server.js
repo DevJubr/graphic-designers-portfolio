02899969cf399ec67621aad5fcf83542d5e3cc53
Jr <jubayerrahman203@gmail.com>
2023-12-11 11:50:13 +0600

    aed server

--- back-end/server.js
+++ back-end/server.js
@@ -6,9 +6,9 @@ app.use(express.static(Path.join(__dirname, "../dist")));
 app.get("*", (req, res) => {
   res.sendFile(Path.resolve(__dirname, "../", "dist", "index.html"));
 });
-app.get("/", (req, res) => {
-  res.send("<h1>hello world!</h1>");
-});
+// app.get("/", (req, res) => {
+//   res.send("<h1>hello world!</h1>");
+// });
 
 app.listen(5000, () => {
   console.log("app listen on port 5000");
