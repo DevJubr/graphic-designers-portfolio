85005fe16e1e64c3b4247a64d13172d8f9e28abc
devjubr <jubayerrahman203@gmail.com>
2024-06-24 17:39:18 +0600

    adding vercel anyletics

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import { Analytics } from "@vercel/analytics/react";
 
 import "@mantine/core/styles.css";
 import "./index.css";
 import { MantineProvider } from "@mantine/core";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <MantineProvider>
+    <Analytics />
     <App />
   </MantineProvider>
 );
